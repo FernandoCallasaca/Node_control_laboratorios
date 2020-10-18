aa4da4160c6e3c6bd3279b89a4c5338771047b6e
Fernando Callasaca <fernandocallasaca@outlook.com>
2020-10-18 18:24:13 -0500

    Bueno regresé a la normalidad el nombre de la base de datos

--- common/appsettings.js
+++ common/appsettings.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'control_laboratorios_v2',
+  database: 'control_laboratorios', // Regresó a su normalidad
   password: '123',
   port: 5432,
-})  
+})
 
 module.exports={
     pool
