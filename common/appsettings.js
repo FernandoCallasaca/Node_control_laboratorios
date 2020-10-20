80df3d59f70675805ff464ef044b30ab616827f0
Fernando Callasaca <fernandocallasaca@outlook.com>
2020-10-20 06:22:19 -0500

    regresando a la normalidad

--- common/appsettings.js
+++ common/appsettings.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'control_laboratorios',
+  database: 'control_laboratorios', // Regresó a su normalidad
   password: '123',
   port: 5432,
 })
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const dbDashboard = require('./dal/dashboard')
 const dbMovil = require('./dal/movil')
 
 const app = express()
-const port = 3200
+const port = 3000
 
 
 app.use(bodyParser.json({ limit: '1000mb' }));
@@ -95,6 +95,114 @@ app.post('/api/seguridad/get', dbSeguridad.get)
 app.post('/api/seguridad/getrole', dbSeguridad.getrole)
 app.post('/api/seguridad/save', dbSeguridad.save)
 app.post('/api/seguridad/resetearclave', dbSeguridad.resetearclave)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.post('/api/general/get', dbGeneral.get)
+app.post('/api/general/save', dbGeneral.save)
+app.post('/api/general/delete', dbGeneral.deleteEntidad)
+app.post('/api/general/getusuarioubigeo', dbGeneral.getUsuarioUbigeo)
+app.post('/api/general/getperiodo', dbGeneral.getPeriodo)
+app.post('/api/general/saveusuarioubigeo', dbGeneral.saveUsuarioUbigeo)
+app.post('/api/general/deleteusuarioubigeo', dbGeneral.deleteUsuarioUbigeo)
+app.post('/api/general/getmantenimiento', dbGeneral.getMantenimiento)
+app.post('/api/general/asignartodo', dbGeneral.asignartodo)
+app.post('/api/general/getperiodoforedit', dbGeneral.getPeriodoForEdit)
+app.post('/api/general/deleteperiodo', dbGeneral.deletePeriodo)
+app.post('/api/general/saveperiodo', dbGeneral.savePeriodo)
+app.post('/api/general/getlocalizationforedit', dbGeneral.getLocalizationForEdit)
+app.post('/api/general/deletelocalization', dbGeneral.deleteLocalization)
+app.post('/api/general/savelocalization', dbGeneral.saveLocalization)
+app.post('/api/general/getunidadservicioforedit', dbGeneral.getUnidadServicioForEdit)
+app.post('/api/general/deleteunidadservicio', dbGeneral.deleteUnidadServicio)
+app.post('/api/general/saveunidadservicio', dbGeneral.saveUnidadServicio)
+app.post('/api/general/gettipoimagen', dbGeneral.getTipoImagenForEdit)
+app.post('/api/general/deletetipoimagen', dbGeneral.deleteTipoImagen)
+app.post('/api/general/savetipoimagen', dbGeneral.saveTipoImagen)
+app.post('/api/general/getelementoprocedenciaforedit', dbGeneral.getElementoProcedenciaForEdit)
+app.post('/api/general/deleteelementoprocedencia', dbGeneral.deleteElementoProcedencia)
+app.post('/api/general/saveelementoprocedencia', dbGeneral.saveElementoProcedencia)
+app.post('/api/general/getelementogrupoforedit', dbGeneral.getElementoGrupoForEdit)
+app.post('/api/general/deleteelementogrupo', dbGeneral.deleteElementoGrupo)
+app.post('/api/general/saveelementogrupo', dbGeneral.saveElementoGrupo)
+app.post('/api/general/getelementoespecificacionforedit', dbGeneral.getElementoEspecificacionForEdit)
+app.post('/api/general/deleteelementoespecificacion', dbGeneral.deleteElementoEspecificacion)
+app.post('/api/general/saveelementoespecificacion', dbGeneral.saveElementoEspecificacion)
+app.post('/api/general/gettipoforespecificacion', dbGeneral.getTipoForEspecificacion)
+app.post('/api/general/getmarcaforespecificacion', dbGeneral.getMarcaForEspecificacion)
+app.post('/api/general/getmodeloforespecificacion', dbGeneral.getModeloForEspecificacion)
+app.post('/api/general/getsoportetipoforedit', dbGeneral.getSoporteTipoForEdit)
+app.post('/api/general/deletesoportetipo', dbGeneral.deleteSoporteTipo)
+app.post('/api/general/savesoportetipo', dbGeneral.saveSoporteTipo)
+app.post('/api/general/getatributoforedit', dbGeneral.getAtributoForEdit)
+app.post('/api/general/deleteatributo', dbGeneral.deleteAtributo)
+app.post('/api/general/saveatributo', dbGeneral.saveAtributo)
+app.post('/api/general/gettipocorteforedit', dbGeneral.getTipoCorteForEdit)
+app.post('/api/general/deletetipocorte', dbGeneral.deleteTipoCorte)
+app.post('/api/general/savetipocorte', dbGeneral.saveTipoCorte)
+app.post('/api/general/gettiposfvforedit', dbGeneral.getTipoSFVForEdit)
+app.post('/api/general/deletetiposfv', dbGeneral.deleteTipoSFV)
+app.post('/api/general/savetiposfv', dbGeneral.saveTipoSFV)
+app.post('/api/general/getmoduloforedit', dbGeneral.getModuloForEdit)
+app.post('/api/general/deletemodulo', dbGeneral.deleteModulo)
+app.post('/api/general/savemodulo', dbGeneral.saveModulo)
+app.post('/api/general/getreparto', dbGeneral.getReparto)
+app.post('/api/general/imprimirreparto', dbGeneral.imprimirReparto)
+app.post('/api/general/getdetallereparto', dbGeneral.getDetalleReparto)
+
+
+
+
+/* Mapa */
+app.post('/api/mapa/get', dbMapa.get)
+app.post('/api/mapa/getxls', dbMapa.getxls)
+app.post('/api/mapa/getdepartamento', dbMapa.getDepartamento)
+app.post('/api/mapa/getprovincia', dbMapa.getProvincia)
+app.post('/api/mapa/getdistrito', dbMapa.getDistrito)
+app.post('/api/mapa/getcentropoblado', dbMapa.getCentroPoblado)
+app.post('/api/mapa/getentidad', dbMapa.getEntidad)
+app.post('/api/mapa/getdetalle', dbMapa.getdetalle)
+app.post('/api/mapa/getrespuesta', dbMapa.getRespuesta)
+app.post('/api/mapa/getlamparas', dbMapa.getLamparas)
+app.post('/api/mapa/getequipos', dbMapa.getEquipos)
+app.post('/api/mapa/getcomponentes', dbMapa.getComponentes)
+app.post('/api/mapa/getreclamo', dbMapa.getReclamo)
+app.post('/api/mapa/getdetallereclamo', dbMapa.getDetalleReclamo)
+app.post('/api/mapa/getreclamosgrilla', dbMapa.getReclamosGrilla)
+app.post('/api/mapa/getmotivoreclamo', dbMapa.getMotivoReclamo)
+app.post('/api/mapa/getdataformato', dbMapa.getDataFormato)
+app.post('/api/mapa/getrespuestaall', dbMapa.getRespuestaAll)
+app.post('/api/mapa/getequipoall', dbMapa.getEquiposAll)
+app.post('/api/mapa/getdetalleall', dbMapa.getdetalleAll)
+app.post('/api/mapa/getlamparasall', dbMapa.getLamparasAll)
+app.post('/api/mapa/getcomponentesall', dbMapa.getComponentesAll)
+app.post('/api/mapa/getdataformatoreclamo', dbMapa.getDataFormatoReclamo)
+app.post('/api/mapa/getdetallereclamoall', dbMapa.getdetalleReclamoAll)
+app.post('/api/mapa/getdetallerepartoall', dbMapa.getdetalleRepartoAll)
+app.post('/api/mapa/getxlsreparto', dbMapa.getxlsReparto)
+app.post('/api/mapa/getcnxcausa', dbMapa.getCnxCausa)
+app.post('/api/mapa/getequiposrobosuministro', dbMapa.getEquiposRoboSuministro)
+/* Movil */
+app.get('/api/movil/get', dbMovil.get)
+
+/* Dashboar */
+app.post('/api/dashboard/getfecha', dbDashboard.getFecha)
+app.post('/api/dashboard/getdepartamento', dbDashboard.getDepartamento)
+app.post('/api/dashboard/getipomantenimiento', dbDashboard.getTipoMantenimiento)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
